b564692d1cb93b1462e5310920f396da0c53a552
Xiaoke <shock.jiang@gmail.com>
2026-04-02 09:46:47 +0800

    update favicon

--- _build.py
+++ _build.py
@@ -224,6 +224,7 @@ html = f'''<!DOCTYPE html>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>{profile['name']['en']}</title>
+  <link rel="icon" href="assets/images/favicon.png" type="image/png">
   <style>{css}</style>
 </head>
 <body>
